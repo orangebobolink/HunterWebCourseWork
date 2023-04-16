927a9a229920cfd58e0f9581c61f071614eae522
Osten Power <osten_power@mail.ru>
2023-04-16 09:27:44 +0300

    interface IRepository

---
+++ services/asp-web-apo/DAL/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace DAL.Interfaces
+{
+    public interface IRepository<T> : IDisposable
+        where T : class
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T> GetById(int id);
+        public Task AddAsync(T item);
+        public Task Update(T item);
+        public Task Remove(int id);
+        public Task SaveChangesAsync();
+    }
+}
