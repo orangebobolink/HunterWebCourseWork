6b58e4c45807d1d9711a4a378788a539d98f6026
Osten Power <osten_power@mail.ru>
2023-04-17 15:12:32 +0300

    realized userReposiotry

---
+++ services/asp-web-apo/DAL/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Entities.UserEntities;
+using DAL.Interfaces;
+
+namespace DAL.Repositories.UserRepository
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        public Task<IEnumerable<User>> GetAllIncludeDetailAsync();
+        public Task<User?> GetByIdIncludeDetailAsync(int id);
+    }
+}
---
+++ services/asp-web-apo/DAL/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using DAL.Data;
+using DAL.Entities.UserEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories.UserRepository
+{
+    internal class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async override Task<IEnumerable<User>> GetAll()
+             => await _dbContext.Users
+                .AsNoTracking()
+                .ToListAsync();
+
+        public async Task<IEnumerable<User?>> GetAllIncludeDetailAsync()
+             => await _dbContext.Users
+                .Include(u => u.UserDetail)
+                .Include(u => u.UserDetail.Messanger)
+                .AsNoTracking()
+                .ToListAsync();
+
+        public async override Task<User?> GetById(int id)
+            => await _dbContext.Users
+
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+        public async Task<User?> GetByIdIncludeDetailAsync(int id)
+            => await _dbContext.Users
+                .Include(u => u.UserDetail)
+                .Include(u => u.UserDetail.Messanger)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == id);
+    }
+}
