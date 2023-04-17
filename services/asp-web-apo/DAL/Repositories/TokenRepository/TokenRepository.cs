5db8716c65a94f7e6f6ce625d144f02f29042290
Osten Power <osten_power@mail.ru>
2023-04-17 15:40:39 +0300

    realized tokenRepository

---
+++ services/asp-web-apo/DAL/Repositories/TokenRepository/TokenRepository.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Data;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories.TokenRepository
+{
+    internal class TokenRepository : BaseRepository<Token>, ITokenRepository
+    {
+        public TokenRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async override Task<IEnumerable<Token>> GetAll()
+            => await _dbContext.Tokens
+                .Include(t => t.User)
+                .AsNoTracking()
+                .ToListAsync();
+
+        public async override Task<Token?> GetById(int id)
+             => await _dbContext.Tokens
+                .Include(t => t.User)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(t => t.Id == id);
+    }
+}
