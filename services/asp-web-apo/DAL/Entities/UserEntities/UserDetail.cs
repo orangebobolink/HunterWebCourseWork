c5281e057e98abde5f1b5319890d3d60d0549848
Osten Power <osten_power@mail.ru>
2023-04-17 16:38:27 +0300

    realized user

---
+++ services/asp-web-apo/DAL/Entities/UserEntities/UserDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL.Entities.UserEntities
+{
+    public class UserDetail
+    {
+        public int Id { get; set; }
+        public int? MessangerId { get; set; }
+        public Messanger? Messanger { get; set; }
+
+    }
+}
