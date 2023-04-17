2b248d22381c3eb8fef8522dff0111a74ee98f1e
Osten Power <osten_power@mail.ru>
2023-04-17 16:06:31 +0300

    create dal configuration for di

---
+++ services/asp-web-apo/DAL/Configurations/DefaultConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using DAL.Data;
+using DAL.Repositories.AnimalRepository;
+using DAL.Repositories.GenderRepository;
+using DAL.Repositories.HuntingSeasonRepository;
+using DAL.Repositories.MessangerRepository;
+using DAL.Repositories.OrderRepository;
+using DAL.Repositories.TokenRepository;
+using DAL.Repositories.UserRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL.Configurations
+{
+    public static class DefaultConfiguration
+    {
+        public static void ConfurationDataAccess(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.ConfigurationMSSQLServer(configuration);
+            service.ConfigurationRepositories();
+        }
+
+        private static void ConfigurationMSSQLServer(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("DAL")));
+        }
+
+        private static void ConfigurationRepositories(this IServiceCollection service)
+        {
+            service.AddScoped<IAnimalRepository, AnimalRepository>();
+            service.AddScoped<IGenderRepository, GenderRepository>();
+            service.AddScoped<IHuntingSeasonRepository, HuntingSeasonReposiptoty>();
+            service.AddScoped<IMessangerRepository, MessangerReposiotry>();
+            service.AddScoped<IOrderRepository, OrderRepository>();
+            service.AddScoped<ITokenRepository, TokenRepository>();
+            service.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
