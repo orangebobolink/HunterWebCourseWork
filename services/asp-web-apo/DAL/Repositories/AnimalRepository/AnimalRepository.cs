2a6d1dc647e12d686e687caa393d4e6768d403f4
Osten Power <osten_power@mail.ru>
2023-04-17 14:17:34 +0300

    realization animalReposiotry

---
+++ services/asp-web-apo/DAL/Repositories/AnimalRepository/AnimalRepository.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Data;
+using DAL.Entities.AnimalEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories.AnimalRepository
+{
+    internal class AnimalRepository : BaseRepository<Animal>, IAnimalRepository
+    {
+        public AnimalRepository(ApplicationContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public async override Task<IEnumerable<Animal>> GetAll()
+            => await _dbContext.Animals
+            .AsNoTracking()
+            .ToListAsync();
+
+        public async Task<IEnumerable<Animal>> GetAllWithDetailsAsync()
+            => await _dbContext.Animals
+            .Include(a => a.AnimalDetail)
+            .AsNoTracking()
+            .ToListAsync();
+
+        public async override Task<Animal?> GetById(int id)
+            => await _dbContext.Animals
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        public async Task<Animal?> GetByIdWithDetailsAsync(int id)
+            => await _dbContext.Animals
+            .Include(a => a.AnimalDetail)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
---
+++ services/asp-web-apo/DAL/Repositories/AnimalRepository/IAnimalRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Entities.AnimalEntities;
+using DAL.Interfaces;
+
+namespace DAL.Repositories.AnimalRepository
+{
+    public interface IAnimalRepository : IRepository<Animal>
+    {
+        public Task<IEnumerable<Animal>> GetAllWithDetailsAsync();
+        public Task<Animal?> GetByIdWithDetailsAsync(int id);
+    }
+}
