7cf9676e578f3032f1fd838a6610a79fa1eace10
Osten Power <osten_power@mail.ru>
2023-04-18 16:17:10 +0300

    create demo context

--- services/asp-web-apo/DAL/Data/ApplicationContext.cs
+++ services/asp-web-apo/DAL/Data/ApplicationContext.cs
@@ -1,6 +1,7 @@
 ﻿using DAL.Entities;
 using DAL.Entities.AnimalEntities;
 using DAL.Entities.HuntingSeasonEntities;
+using DAL.Entities.UserEntities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Data
@@ -13,5 +14,37 @@ namespace DAL.Data
         public DbSet<Token> Tokens { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Messanger> Messangers { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(user => user.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<UserDetail>()
+               .HasIndex(user => user.Phone)
+               .IsUnique();
+
+            modelBuilder.Entity<Animal>()
+               .HasIndex(animal => animal.Name)
+               .IsUnique();
+
+            modelBuilder.Entity<Gender>()
+               .HasIndex(gender => gender.Name)
+               .IsUnique();
+
+            modelBuilder.Entity<MethodOfHunting>()
+              .HasIndex(method => method.Name)
+              .IsUnique();
+
+            modelBuilder.Entity<TypeOfHunting>()
+              .HasIndex(type => type.Name)
+              .IsUnique();
+
+            modelBuilder.Entity<Messanger>()
+              .HasIndex(messanger => messanger.Name)
+              .IsUnique();
+        }
     }
 }
