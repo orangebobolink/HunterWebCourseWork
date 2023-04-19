10fa7b0b433088b4d4f301be8e128e6f91efb8a2
Osten Power <osten_power@mail.ru>
2023-04-19 17:29:37 +0300

    add 3 dtos

---
+++ services/asp-web-apo/BLL/DTOs/MethodOfHuntingDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BLL.DTOs
+{
+    public class MethodOfHuntingDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ services/asp-web-apo/BLL/DTOs/TypeOfHuntingDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BLL.DTOs
+{
+    internal class TypeOfHuntingDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
