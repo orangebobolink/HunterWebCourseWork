002757270c059203512d3948542c744d33eff698
Osten Power <osten_power@mail.ru>
2023-04-19 17:29:49 +0300

    add IRepositoryService

---
+++ services/asp-web-apo/BLL/Interfaces/IRepositoryService.cs
@@ -0,0 +1,12 @@
+﻿namespace BLL.Interfaces
+{
+    public interface IRepositoryService<T>
+        where T : class
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T?> GetById(int id);
+        public Task<T> Add(T item);
+        public Task<T> Update(T item);
+        public Task<T> Remove(T item);
+    }
+}
