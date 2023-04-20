d3c0eef3f0e34852398c16c7726fa343dd28886a
Osten Power <osten_power@mail.ru>
2023-04-20 15:16:39 +0300

    add getbyname to repositpry

--- services/asp-web-apo/DAL/Repositories/UserRepository/IUserRepository.cs
+++ services/asp-web-apo/DAL/Repositories/UserRepository/IUserRepository.cs
@@ -3,7 +3,7 @@ using DAL.Interfaces;
 
 namespace DAL.Repositories.UserRepository
 {
-    public interface IUserRepository : IRepository<User>
+    public interface IUserRepository : IRepository<User>, IGetByEmailRepository<User>
     {
         public Task<IEnumerable<User>> GetAllIncludeDetailAsync();
         public Task<User?> GetByIdIncludeDetailAsync(int id);
