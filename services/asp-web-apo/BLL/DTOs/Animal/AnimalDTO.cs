185638b4798174b396dd02715ea41d8609894b1f
Osten Power <osten_power@mail.ru>
2023-04-20 09:45:32 +0300

    create animalService

---
+++ services/asp-web-apo/BLL/DTOs/Animal/AnimalDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BLL.DTOs.Animal
+{
+    public class AnimalDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
