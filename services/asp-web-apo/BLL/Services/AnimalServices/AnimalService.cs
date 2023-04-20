6f8416945fd67a961744f628285892e09cb9b424
Osten Power <osten_power@mail.ru>
2023-04-20 09:46:36 +0300

    animalservice

---
+++ services/asp-web-apo/BLL/Services/AnimalServices/AnimalService.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using BLL.DTOs.Animal;
+using BLL.Exceptions;
+using DAL.Entities.AnimalEntities;
+using DAL.Repositories.AnimalRepository;
+using Microsoft.Extensions.Logging;
+
+namespace BLL.Services.AnimalServices
+{
+    internal class AnimalService : IAnimalService
+    {
+        private IAnimalRepository _animalRepository;
+        private IMapper _mapper;
+        private ILogger<AnimalService> _logger;
+
+        public AnimalService(IAnimalRepository animalRepository, IMapper mapper, ILogger<AnimalService> logger)
+        {
+            _animalRepository = animalRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<AnimalDetailDTO> AddAsync(AnimalDetailDTO item)
+        {
+            var animalChecked = _animalRepository.GetByNameAsync(item.Name);
+
+            if(animalChecked is not null)
+            {
+                _logger.LogError("");
+
+                throw new NotFoundException("This name is already used");
+            }
+
+            var mapperModel = _mapper.Map<Animal>(item);
+
+            _animalRepository.Add(mapperModel);
+            await _animalRepository.SaveChangesAsync();
+
+            return item;
+        }
+
+        public async Task<IEnumerable<AnimalDTO>> GetAllAsync()
+        {
+            var animalsChecked = await _animalRepository.GetAllAsync();
+
+            if(animalsChecked is null)
+            {
+                _logger.LogError("");
+
+                throw new NotFoundException("There is not data yet");
+            }
+
+            var mapperModel = _mapper.Map<IEnumerable<AnimalDTO>>(animalsChecked);
+
+            return mapperModel;
+        }
+
+        public async Task<AnimalDetailDTO?> GetByIdAsync(int id)
+        {
+            var animalChecked = await _animalRepository.GetByIdAsync(id);
+
+            if(animalChecked is null)
+            {
+                _logger.LogError("");
+
+                throw new NotFoundException("There is not data yet");
+            }
+
+            var mapperModel = _mapper.Map<AnimalDetailDTO>(animalChecked);
+
+            return mapperModel;
+        }
+
+        public async Task<AnimalDetailDTO> RemoveAsync(AnimalDetailDTO item)
+        {
+            var mapperModel = _mapper.Map<Animal>(item);
+
+            _animalRepository.Remove(mapperModel);
+            await _animalRepository.SaveChangesAsync();
+
+            return item;
+        }
+
+        public async Task<AnimalDetailDTO> UpdateAsync(AnimalDetailDTO item)
+        {
+            var mapperModel = _mapper.Map<Animal>(item);
+
+            _animalRepository.Update(mapperModel);
+            await _animalRepository.SaveChangesAsync();
+
+            return item;
+        }
+    }
+}
