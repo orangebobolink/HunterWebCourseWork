1d984330024ec651556f413ca0f52b169651b37b
Osten Power <osten_power@mail.ru>
2023-04-20 09:45:47 +0300

    create huntingSeasonService

---
+++ services/asp-web-apo/BLL/DTOs/HuntingSeason/HuntingSeasonDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BLL.DTOs.HuntingSeason
+{
+    public class HuntingSeasonDTO
+    {
+        public int Id { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+    }
+}
---
+++ services/asp-web-apo/BLL/DTOs/HuntingSeason/HuntingSeasonDetailDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace BLL.DTOs.HuntingSeason
+{
+    public class HuntingSeasonDetailDTO
+    {
+        public int Id { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string TypeName { get; set; } = string.Empty;
+        public string MethodName { get; set; } = string.Empty;
+        public string HuntingTime { get; set; } = string.Empty;
+        public string GenderName { get; set; } = string.Empty;
+        public string Age { get; set; } = string.Empty;
+    }
+}
