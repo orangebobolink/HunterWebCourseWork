755d4780f5965a197e2cb8fa6b3faa81296b6822
Osten Power <osten_power@mail.ru>
2023-04-21 09:50:27 +0300

    create automapper for easier profile

---
+++ services/asp-web-apo/BLL/Mappings/MessangerProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.Entities;
+
+namespace BLL.Mappings
+{
+    internal class MessangerProfile : Profile
+    {
+        public MessangerProfile()
+        {
+            CreateMap<Messanger, MessangerDTO>().ReverseMap();
+        }
+    }
+}
---
+++ services/asp-web-apo/BLL/Mappings/TokenProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.Entities;
+
+namespace BLL.Mappings
+{
+    internal class TokenProfile : Profile
+    {
+        public TokenProfile()
+        {
+            CreateMap<Token, TokenDTO>().ReverseMap();
+        }
+    }
+}
