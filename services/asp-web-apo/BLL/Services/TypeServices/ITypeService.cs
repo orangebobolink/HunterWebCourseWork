b0ad393c01cbd022e245ffb8c5f293b42d6b1c93
Osten Power <osten_power@mail.ru>
2023-04-21 09:27:07 +0300

    add typeservice

---
+++ services/asp-web-apo/BLL/Services/TypeServices/ITypeService.cs
@@ -0,0 +1,9 @@
+﻿using BLL.DTOs;
+using BLL.Interfaces;
+
+namespace BLL.Services.TypeServices
+{
+    public interface ITypeService : IRepositoryService<TypeOfHuntingDTO>, IGetByNameService<TypeOfHuntingDTO>
+    {
+    }
+}
