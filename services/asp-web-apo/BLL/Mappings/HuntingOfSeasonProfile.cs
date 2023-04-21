f2617632b61e7749e9345bb82ab9c5095a3ddc71
Osten Power <osten_power@mail.ru>
2023-04-21 10:08:08 +0300

    add auto mapping for 4 dtos

---
+++ services/asp-web-apo/BLL/Mappings/HuntingOfSeasonProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BLL.DTOs.HuntingSeason;
+using DAL.Entities.HuntingSeasonEntities;
+
+namespace BLL.Mappings
+{
+    internal class HuntingOfSeasonProfile : Profile
+    {
+        public HuntingOfSeasonProfile()
+        {
+            CreateMap<HuntingSeason, HuntingSeasonDTO>().ReverseMap();
+        }
+    }
+}
---
+++ services/asp-web-apo/BLL/Mappings/MethodOfHuntingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BLL.DTOs.HuntingSeason;
+using DAL.Entities.HuntingSeasonEntities;
+
+namespace BLL.Mappings
+{
+    internal class MethodOfHuntingProfile : Profile
+    {
+        public MethodOfHuntingProfile()
+        {
+            CreateMap<MethodOfHunting, MethodOfHuntingDTO>().ReverseMap();
+        }
+    }
+}
